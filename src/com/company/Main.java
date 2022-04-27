7c47dd5912953ec2403e74e55dc6c23ef277ed8b
Victorchelsea <Victorchelsea63@gmail.com>
2022-04-27 16:10:37 +0300

    program for LabWork 01:

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        double gravity = -9.81; // Earth's gravity in m/s^2
+        // Earth's gravity in m/s^2
+        double gravity = -9.81;
+        //initial velocity in m/s
         double initialVelocity = 0.0;
+        // time in s
         double fallingTime = 10.0;
+        //position in m
         double initialPosition = 0.0;
-        double finalPosition = 0.0;
+        //final position in m
+        double finalPosition;
+        //formulae
+        finalPosition = (0.5 * (gravity*fallingTime*fallingTime) + (initialVelocity*fallingTime) + initialPosition);
+        //output
         System.out.println("The object's position after " + fallingTime +
                 " seconds is " + finalPosition + " m.");
     }
